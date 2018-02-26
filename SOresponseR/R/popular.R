4156b88c688bb809c150d2ed957d6f494f85559f
tedd <thomp114@uwindsor.com>
2018-02-26 11:37:34 -0800

    Proposal Issue fixes

---
+++ SOresponseR/R/popular.R
@@ -0,0 +1,12 @@
+popular <- function(question) {
+  # '''
+  # Loads Returns most popular response on webpage.
+  #
+  # Parameters:
+  #     question: An object that used the `web_page()` function.
+  # Returns:
+  #     reponse: Returns a response. This will be of a character type.
+  # '''
+
+
+}
---
+++ SOresponseR/R/web_page.R
@@ -0,0 +1,20 @@
+web_page <- function(url) {
+  # '''
+  # Loads Stack Overflow response webpage.
+  #
+  # Parameters:
+  #     url: Full URL to stack overflow response webpage. Character type.
+  # Returns:
+  #     q: The question being asked on the given Stack Overflow response webpage. Character type. If unsuccessful corisponding error will be returned.
+  # '''
+
+  #library(xml2)
+  #library(rvest)
+  response_page <- read_html(url)
+  question <- response_page %>%
+    html_nodes('#question-header .question-hyperlink') %>%
+    html_text()
+
+  print(question)
+  return(url)
+}
