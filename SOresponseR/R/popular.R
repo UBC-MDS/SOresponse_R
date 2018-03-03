e518fb2cc2e52114acfcf17d932936f193066e88
tedd <thomp114@uwindsor.com>
2018-03-03 14:38:18 -0800

    documentation fix

--- SOresponseR/R/popular.R
+++ SOresponseR/R/popular.R
@@ -1,13 +1,15 @@
-popular <- function(url, types="likes") {
-  # '''
-  # Loads Returns most popular response on webpage.
-  #
-  # Parameters:
-  #     url: An object that used the `web_page()` function.
-  #     type: "likes" returns response with the most likes. "author" returns response confirmed by author of question.
-  # Returns:
-  #     reponse: Returns a response. This will be of a character type.
-  # '''
-
+#' popular(url, type="likes")
+#'
+#'Loads Returns most popular response on webpage.
+#'
+#' @param url An object that used the `web_page()` function.
+#' @param type "likes" returns response with the most likes. "author" returns response confirmed by author of question.
+#'
+#' @return reponse Returns a response. This will be of a character type.
+#' @export
+#'
+#' @examples
+#'#'popular('https://stackoverflow.com/questions/3505701/grouping-functions-tapply-by-aggregate-and-the-apply-family', type="likes")
+popular <- function(url, type="likes") {
 
 }
--- SOresponseR/R/web_page.R
+++ SOresponseR/R/web_page.R
@@ -1,12 +1,15 @@
+#' web_page(url)
+#'
+#'Loads Stack Overflow response webpage.
+#'
+#' @param url Full URL to stack overflow response webpage. Character type.
+#'
+#' @return q The question being asked on the given Stack Overflow response webpage. Character type. If unsuccessful corresponding error will be returned.
+#' @export
+#'
+#' @examples
+#' #'web_page('https://stackoverflow.com/questions/3505701/grouping-functions-tapply-by-aggregate-and-the-apply-family')
 web_page <- function(url) {
-  # '''
-  # Loads Stack Overflow response webpage.
-  #
-  # Parameters:
-  #     url: Full URL to stack overflow response webpage. Character type.
-  # Returns:
-  #     q: The question being asked on the given Stack Overflow response webpage. Character type. If unsuccessful corresponding error will be returned.
-  # '''
 
 
 }
