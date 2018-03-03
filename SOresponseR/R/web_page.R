3f59ec719a734e6485702126fae869bad5b96416
tedd <thomp114@uwindsor.com>
2018-03-03 13:38:51 -0800

    documentation

--- SOresponseR/R/web_page.R
+++ SOresponseR/R/web_page.R
@@ -5,16 +5,8 @@ web_page <- function(url) {
   # Parameters:
   #     url: Full URL to stack overflow response webpage. Character type.
   # Returns:
-  #     q: The question being asked on the given Stack Overflow response webpage. Character type. If unsuccessful corisponding error will be returned.
+  #     q: The question being asked on the given Stack Overflow response webpage. Character type. If unsuccessful corresponding error will be returned.
   # '''
 
-  #library(xml2)
-  #library(rvest)
-  response_page <- xml2::read_html(url) # Loads URL
-  question <- response_page %>% # Pulls question
-    rvest::html_nodes('#question-header .question-hyperlink') %>%
-    rvest::html_text()
 
-  print(question) #prints question
-  return(url) # returns url
 }
